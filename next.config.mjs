ed7e063a3399c8294773b0a02567e7e0d1ebe09f
ewzxyh <yoshidaenzo@hotmail.com>
2025-12-13 02:23:54 -0300

    Atualiza dependências e configurações do projeto: altera a versão do Next.js para ^16.0.10, modifica os scripts de desenvolvimento e build para usar turbopack, remove configurações experimentais do Next.js e ajusta o tsconfig.json para incluir novos tipos e melhorar a compatibilidade.

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
   },
 }
 
